411b3169e94efdf974d811c0ce59f33766a6d367
lorenzo <126621314+apptrackbefasion@users.noreply.github.com>
2023-10-27 14:51:46 -0700

    Se hace subida de las rutas para devolver informacion sobre las unidades base y la marca en la app del crm

--- SAP_API/Models/SAPContext.cs
+++ SAP_API/Models/SAPContext.cs
@@ -21,9 +21,9 @@ namespace SAP_API.Models
             //oCompany.CompanyDB = "CCFN_PROD";szzzzzz
             //oCompany.CompanyDB = "CCFN_CORPORATIVO";
             //oCompany.CompanyDB = "CCFN_DEV";
-            //oCompany.CompanyDB = "CCFN_B1CORP";
+            oCompany.CompanyDB = "CCFN_B1CORP";
             //oCompany.CompanyDB = "CCFN_BASECORP";
-            oCompany.CompanyDB = "CCFN_DEV";
+            //oCompany.CompanyDB = "CCFN_DEV";
 
             oCompany.UserName = "SISTEMAS04";
             oCompany.Password = "SAP1234";
